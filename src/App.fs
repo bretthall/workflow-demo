073f3531e67f0fe71a7eac3cee4040573972ce3a
Brett Hall <bretthall@fastmail.fm>
2019-08-06 21:29:50 -0700

    A little cleanup

--- src/App.fs
+++ src/App.fs
@@ -1,10 +1,5 @@
 module App
 
-(**
- The famous Increment/Decrement ported from Elm.
- You can find more info about Elmish architecture and samples at https://elmish.github.io/
-*)
-
 open Elmish
 open Elmish.React
 open Fable.Helpers.React
