428e9a186fe1ea96fd6d9ad3f352fb297d4cdd8b
Brett Hall <bretthall@fastmail.fm>
2019-09-09 16:00:40 -0700

    Added exercise1 solution

--- workflow/workflows.fs
+++ workflow/workflows.fs
@@ -14,17 +14,26 @@ let reset = workflow {
 
 //TODO: Exercise 1: Fill in the following workflow
 let wait = workflow {
-    do! addControlMsg "Workflow not implemented" //remove this when you've implemented the below
-
     // set the device state to "good"
+    do! setDeviceState Device.Good
     // wait 5 seconds
+    do! wait 5<seconds>
     // set the device state to "bad"
+    do! setDeviceState Device.Bad
     // wait for the data to increment 5 times (you'll need to get the current data value)
+    let! curData = getCurrentData ()
+    let targetData = curData + 5
+    let! finalData = waitForData targetData
     // add a device message saying what the data value is at the end of the wait
+    do! addDeviceMsg (sprintf "final data = %d" finalData)
     // set the device state to "ugly"
+    do! setDeviceState Device.Ugly
     // get input from the device
+    let! input = getDeviceInput "Waiting for input"
     // add a control message saying what the input is
+    do! addControlMsg (sprintf "Got input = %s" input)
     // set the device state to "good"
+    do! setDeviceState Device.Good
 }
 
 let choice = workflow {
